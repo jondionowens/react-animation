498911a8e1888277cbe65e9dad9318c68f9b2b81
Jonathan Owens <jonathano@metia.com>
2018-10-25 16:11:48 -0700

    Pushing up

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CSSTransitionGroup } from 'react-transition-group'
+import Square from './components/Square.jsx'
+import uniqueid from 'uniqueid';
+import Anime from 'react-anime';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
+let App =() => (
+  <Anime opacity={[0, 1]} translateY={'1em'} delay={(e, i) => i * 1000}>
+  <h1>Blog Post</h1>
+  <section>
+    <p>Upon this, Daggoo, with either hand upon the gunwale to steady his way, swiftly slid aft, and then erecting himself volunteered his lofty shoulders for a pedestal.</p>
+    <p>"Good a mast-head as any, sir. Will you mount?"</p>
+    <p>"That I will, and thank ye very much, my fine fellow; only I wish you fifty feet taller."</p>
+  </section>
+</Anime>
+    );
 
-    }
-  }
+let node = document.getElementById('app');
 
-  render() {
-    return (
-      <div>sfdsfs</div>
-    )
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App/>, node);
